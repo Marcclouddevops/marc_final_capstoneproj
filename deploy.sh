78cb5288ca2a7634612c1e86cfb499c18d313cdd
Marcclouddevops <marcclouddevops@gmail.com>
2024-05-31 22:18:47 +0000

    deploy.sh script changes

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-DOCKER_USERNAME="marcsamuel07"
-DOCKER_PASSWORD="dckr_pat_lvLLhQ4zNuygOPjlXsUNgpeewUs"
+# Set your Docker repository details
 DOCKER_REPO="prod"
 DOCKER_TAG="latest"
 
+# Log in to Docker Hub
 echo "Logging in to Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# Tag the Docker image
 LOCAL_IMAGE="marc_proj"
-
 echo "Tagging the Docker image..."
 docker tag "${LOCAL_IMAGE}" "$DOCKER_USERNAME/${DOCKER_REPO}:${DOCKER_TAG}"
 
-
+# Push the Docker image to Docker Hub
 echo "Pushing the Docker image to Docker Hub..."
 docker push "$DOCKER_USERNAME/${DOCKER_REPO}:${DOCKER_TAG}"
 
 echo "Deployment to Docker Hub completed successfully."
 
+# Deploy using Docker Compose
 docker-compose up -d
-
